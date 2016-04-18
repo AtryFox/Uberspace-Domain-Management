253cf49204fbc75b778aaa321262e9ebf1e1d7b4
DerAtrox <code@deratrox.de>
2016-04-19 00:57:41 +0200

    starting PDO_MYSQL implementation

--- domain-management/functions/mysql.php
+++ domain-management/functions/mysql.php
@@ -4,6 +4,15 @@
 
 $mysqli = new mysqli($hostname, $username, $password, $database);
 if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-?>
+
+
+try {
+	$pdo = new PDO('mysql:host=' . $hostname . ';dbname=' . $database, $username, $password);
+} catch (PDOException $e) {
+	print "Error!: " . $e->getMessage() . "<br/>";
+	die();
+}
+
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
